66e90d1ab94aa4dc3a9ff7a9d529a2f27df32624
Finn <gelomi90@gmail.com>
2024-07-11 15:54:59 +0900

    please commit this time...

--- langchain_helper2.py
+++ langchain_helper2.py
@@ -55,3 +55,4 @@ def get_response_from_query(db, query, k=4):
     response = chain.run(question = query, docs = docs_page_content)
     response = response.replace("\n", "")
     return response, docs
+    # for commit
